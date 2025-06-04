1730dbb4e55873d3614be3c63d8577909e98b240
pure-wa <tkdghks8242@gmail.com>
2025-06-04 10:19:58 +0900

    첫번쨰 커밋

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -12,3 +12,12 @@ git commit -m "메세지제목" -m "메세지 내용"
 
 # git commit만 입력하고 엔터시 vi모드 -> 첫줄:타이틀, 두번째줄부터 내용 
 git commit
+
+# commit 이력 확인
+git log
+git log --oneline
+
+# 원격저장소로 업로드
+git push origin 브렌치명
+# 충돌발생시 충돌무시하고, 로컬기준으로 원격으로 덮어쓰기
+git push origin 브렌치명 --force
